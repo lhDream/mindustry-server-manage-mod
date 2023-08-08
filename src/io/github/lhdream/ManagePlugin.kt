f06ecfe6576232a0b97888e3420b3b3bce8a6cba
luhua <luhuadream@foxmail.com>
2023-08-08 14:32:48 +0800

    change plugin

--- src/io/github/lhdream/ManagePlugin.kt
+++ src/io/github/lhdream/ManagePlugin.kt
@@ -1,13 +1,37 @@
 package io.github.lhdream
 
 import arc.Core
+import arc.Events
+import arc.util.CommandHandler
+import mindustry.content.Blocks
+import mindustry.game.EventType.BuildSelectEvent
+import mindustry.gen.Call
 import mindustry.mod.Plugin
 
 class ManagePlugin: Plugin() {
 
     override fun init() {
-        super.init()
-        Core.settings;
+
+        Events.on(BuildSelectEvent::class.java){
+            if(!it.breaking && it.builder?.buildPlan()?.block == Blocks.thoriumReactor && it.builder.isPlayer){
+                Call.sendMessage("scarlet]ALERT![] ${it.builder.player.name} 在[${it.tile.x},${it.tile.y}]位置建造了反应堆")
+            }
+        }
+
+    }
+
+    /**
+     * 服务器命令
+     */
+    override fun registerServerCommands(handler: CommandHandler) {
+
+    }
+
+    /**
+     * 客户端命令
+     */
+    override fun registerClientCommands(handler: CommandHandler) {
+
     }
 
 
