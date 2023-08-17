398850f36c776c43c5ddce04f3805ff60590664f
luhua <luhuadream@foxmail.com>
2023-08-17 11:18:14 +0800

    增加客户端设置下一张地图命令

--- src/io/github/lhdream/ManagePlugin.kt
+++ src/io/github/lhdream/ManagePlugin.kt
@@ -1,23 +1,29 @@
 package io.github.lhdream
 
-import arc.Core
 import arc.Events
 import arc.util.CommandHandler
+import mindustry.Vars.maps
 import mindustry.content.Blocks
 import mindustry.game.EventType.BuildSelectEvent
+import mindustry.game.EventType.CoreChangeEvent
+import mindustry.game.Gamemode
 import mindustry.gen.Call
+import mindustry.gen.Player
 import mindustry.mod.Plugin
+
 
 class ManagePlugin: Plugin() {
 
     override fun init() {
-
         Events.on(BuildSelectEvent::class.java){
             if(!it.breaking && it.builder?.buildPlan()?.block == Blocks.thoriumReactor && it.builder.isPlayer){
-                Call.sendMessage("scarlet]ALERT![] ${it.builder.player.name} 在[${it.tile.x},${it.tile.y}]位置建造了反应堆")
+                Call.sendMessage("${it.builder.player.name} 在[${it.tile.x},${it.tile.y}]位置建造了反应堆")
             }
         }
 
+        Events.on(CoreChangeEvent::class.java){
+
+        }
     }
 
     /**
@@ -31,6 +37,20 @@ class ManagePlugin: Plugin() {
      * 客户端命令
      */
     override fun registerClientCommands(handler: CommandHandler) {
+        handler.register<Player>("nextMap","<mapName>","设置下一张地图"){ args,player ->
+            if(!player.admin){
+                Call.sendMessage("非管理员用户，不可切换地图","系统消息",player)
+                return@register
+            }
+            val res = maps.byName(args[0])
+            if(res != null){
+                maps.shuffleMode.next(Gamemode.survival,res)
+                Call.sendMessage("设置完成","系统消息",player)
+            }else{
+                Call.sendMessage("地图${args[0]}无法找到","系统消息",player)
+            }
+
+        }
 
     }
 
